0b0db2074f178d74e8c82e0004df6a69dff4545e
A-Surkov-2004 <124879961+A-Surkov-2004@users.noreply.github.com>
2026-01-09 06:15:40 +0300

    Update oauth_client.py

--- Pract2/oauth_client.py
+++ Pract2/oauth_client.py
@@ -1,7 +1,7 @@
 from requests_oauthlib import OAuth2Session
 import os
 
-# Получаем из переменных окружения (рекомендуется для безопасности)
+
 CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID", " ")
 CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET", " ")
 REDIRECT_URI = "http://localhost:8000/callback"
@@ -9,23 +9,23 @@ AUTHORIZATION_BASE_URL = "https://github.com/login/oauth/authorize"
 TOKEN_URL = "https://github.com/login/oauth/access_token"
 SCOPE = ["read:user"]
 
-# Создаем сессию без секрета на этом этапе
+
 oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
 authorization_url, state = oauth.authorization_url(AUTHORIZATION_BASE_URL)
 print("Перейдите по ссылке для авторизации:", authorization_url)
 
-# После редиректа получим код авторизации
+
 authorization_response = input("Введите полный URL редиректа: ")
 
-# Получаем токен
+
 token = oauth.fetch_token(
     TOKEN_URL,
     authorization_response=authorization_response,
-    client_secret=CLIENT_SECRET  # Здесь передаем секрет
+    client_secret=CLIENT_SECRET 
 )
 
 print("Токен получен:", token)
 
-# Теперь можно делать запросы к API
+
 user_info = oauth.get("https://api.github.com/user")
 print("Информация о пользователе:", user_info.json())
